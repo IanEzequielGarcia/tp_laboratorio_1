e14668363c49e8ec6e3efc7b0ee768487ec7e841
Ianchoo <54332381+Ianchoo@users.noreply.github.com>
2020-09-06 15:23:20 -0300

    Punto 4

--- TPNUMEROUNO/main.c
+++ TPNUMEROUNO/main.c
@@ -24,6 +24,8 @@ int main()
 {
     int primerNumero;
     int segundoNumero;
+    int tipoOperacion;
+    int loopPregunta=0;
     int resultadoInt;
     float resultadoFloat;
 
@@ -32,16 +34,44 @@ int main()
     printf("Ingrese el segundo numero\n");
     scanf("%d",&segundoNumero);
 
-    resultadoInt= Suma(primerNumero,segundoNumero);
-    printf("El resultado de la suma es %d \n",resultadoInt);
-    resultadoInt= Resta(primerNumero,segundoNumero);
-    printf("El resultado de la resta es %d \n",resultadoInt);
-    resultadoInt= Multiplicacion(primerNumero,segundoNumero);
-    printf("El resultado de la Multiplicacion es %d \n",resultadoInt);
-    resultadoFloat= Division(primerNumero,segundoNumero);
-    printf("El resultado de la Division es %.2f \n",resultadoFloat);
-    resultadoInt= Recursividad(primerNumero);
-    printf("El resultado de la factorizacion del primer numero es %d \n",resultadoInt);
+    while(loopPregunta==0)
+    {
+        printf("Ingrese el tipo de operacion\n");
+        printf("1.Calcular la suma (A+B)\n");
+        printf("2.Calcular la resta (A-B)\n");
+        printf("3.Calcular la multiplicacion (A*B)\n");
+        printf("4.Calcular la division (A/B)\n");
+        printf("5.Calcular el factorial (A!)\n");
+        printf("6.Salir\n");
+        scanf("%d",&tipoOperacion);
+
+        switch(tipoOperacion)
+        {
+            case 1:
+                resultadoInt= Suma(primerNumero,segundoNumero);
+                printf("El resultado de la suma es %d \n",resultadoInt);
+                break;
+            case 2:
+                resultadoInt= Resta(primerNumero,segundoNumero);
+                printf("El resultado de la resta es %d \n",resultadoInt);
+                break;
+            case 3:
+                resultadoInt= Multiplicacion(primerNumero,segundoNumero);
+                printf("El resultado de la Multiplicacion es %d \n",resultadoInt);
+                break;
+            case 4:
+                 resultadoFloat= Division(primerNumero,segundoNumero);
+                 printf("El resultado de la Division es %.2f \n",resultadoFloat);
+                 break;
+            case 5:
+                resultadoInt= Recursividad(primerNumero);
+                printf("El resultado de la factorizacion del primer numero es %d \n",resultadoInt);
+                break;
+            case 6:
+                loopPregunta=1;
+                break;
+        }
+    }
 
     return 0;
 }
