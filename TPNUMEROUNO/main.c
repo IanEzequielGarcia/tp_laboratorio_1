4f55a9351a6a83e1a564fcafc0763efa56840192
Ianchoo <54332381+Ianchoo@users.noreply.github.com>
2020-09-09 19:59:11 -0300

    Algunas validaciones arregladas

--- TPNUMEROUNO/main.c
+++ TPNUMEROUNO/main.c
@@ -24,8 +24,8 @@ int main()
 {
     float primerNumero=0;
     float segundoNumero=0;
-    int factorizarPrimerNumero;
-    int factorizarSegundoNumero;
+    int factorizarPrimerNumero=0;
+    int factorizarSegundoNumero=0;
     int seleccionMenu;
     int loopMenu=0;
     float resultadoSuma;
@@ -34,19 +34,19 @@ int main()
     int resultadoFactorizacionr1;
     int resultadoFactorizacionr2;
     float resultadoFloat;
+    int banderaOperaciones=0;
 
     while(loopMenu==0)
     {
         printf("A=%.2f\n",primerNumero);
         printf("B=%.2f\n",segundoNumero);
-
         printf("1.Ingresar 1er operando\n");
         printf("2.Ingresar 2do operando\n");
         printf("3.Calcular todas las operaciones\n");
         printf("4.Mostar resultados\n");
         printf("5. Salir\n\n");
         scanf("%d",&seleccionMenu);
-         switch(seleccionMenu)
+        switch(seleccionMenu)
         {
             case 1:
                 printf("Ingrese el primer numero\n");
@@ -59,39 +59,58 @@ int main()
                 factorizarSegundoNumero=segundoNumero;
                 break;
             case 3:
+                if(primerNumero==0&&segundoNumero==0)
+                {
+                    printf("\nPor favor, ingrese un valor para A o B primero y reintente \n\n");
+                    break;
+                }
+                else
+                {
                 resultadoSuma= Suma(primerNumero,segundoNumero);
                 resultadoResta= Resta(primerNumero,segundoNumero);
                 resultadoMultiplicacion= Multiplicacion(primerNumero,segundoNumero);
                 resultadoFloat= Division(primerNumero,segundoNumero);
                 resultadoFactorizacionr1= Recursividad(factorizarPrimerNumero);
                 resultadoFactorizacionr2= Recursividad(factorizarSegundoNumero);
+
+                banderaOperaciones=1;
                 break;
+                }
             case 4:
-                printf("El resultado de la suma es %.2f \n",resultadoSuma);
-                printf("El resultado de la resta es %.2f \n",resultadoResta);
-                printf("El resultado de la Multiplicacion es %.2f \n",resultadoMultiplicacion);
-                if(resultadoFloat==0)
+                if(banderaOperaciones==0)
                 {
-                    printf("No se puede dividir por 0\n");
+                    printf("\nPor favor, primero seleccione la opcion 3 y haga las operaciones\n\n");
+                    break;
                 }
                 else
                 {
-                    printf("El resultado de la Division es %.2f \n",resultadoFloat);
+                    printf("\nEl resultado de la suma es %.2f \n",resultadoSuma);
+                    printf("El resultado de la resta es %.2f \n",resultadoResta);
+                    printf("El resultado de la Multiplicacion es %.2f \n",resultadoMultiplicacion);
+                    if(resultadoFloat==0)
+                    {
+                        printf("No se puede dividir por 0\n");
+                    }
+                    else
+                    {
+                        printf("El resultado de la Division es %.2f \n",resultadoFloat);
+                    }
+                    if(resultadoFactorizacionr1==0||resultadoFactorizacionr2==0)
+                    {
+                        printf("haga el factorial de un numero mayor a 0\n");
+                    }
+                    else
+                    {
+                        printf("El resultado de la factorizacion del primer numero es %d \n",resultadoFactorizacionr1);
+                        printf("El resultado de la factorizacion del segundo numero es %d \n\n",resultadoFactorizacionr2);
+                    }
+                    break;
+                case 5:
+                    printf("\nMuchas gracias por usar la pseudoCalculadora, hasta la proxima\n");
+                    loopMenu=1;
+                    break;
                 }
-                if(resultadoFactorizacionr1==0||resultadoFactorizacionr2==0)
-                {
-                    printf("haga el factorial de un numero mayor a 0\n");
-                }
-                else
-                {
-                    printf("El resultado de la factorizacion del primer numero es %d \n",resultadoFactorizacionr1);
-                    printf("El resultado de la factorizacion del segundo numero es %d \n",resultadoFactorizacionr2);
-                }
-                break;
-            case 5:
-                printf("Muchas gracias por usar la pseudoCalculadora, hasta la proxima");
-                loopMenu=1;
-                break;
+
         }
     }
 
