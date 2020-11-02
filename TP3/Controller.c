54b74aa2c061a65adb9f8fdcb63953b2b620f6db
Ianchoo <54332381+Ianchoo@users.noreply.github.com>
2020-11-01 22:21:10 -0300

    TP3 terminado

--- TP3/Controller.c
+++ TP3/Controller.c
@@ -7,65 +7,226 @@
 
 int controller_loadFromText(char* path, LinkedList* pArrayListEmployee)
 {
-    return 1;
+    int sePudo=0;
+    if(path!=NULL)
+    {
+        sePudo++;
+    }
+    if(parser_EmployeeFromText(path, pArrayListEmployee))
+    {
+       sePudo++;
+    }
+    return sePudo;
 }
 
 
 int controller_loadFromBinary(char* path, LinkedList* pArrayListEmployee)
 {
-    return 1;
+    int sePudo=0;
+    if(path!=NULL)
+    {
+        sePudo++;
+    }
+    if(parser_EmployeeFromBinary(path, pArrayListEmployee))
+    {
+       sePudo++;
+    }
+    return sePudo;
 }
 
 
 int controller_addEmployee(LinkedList* pArrayListEmployee)
 {
+    int len,i;
+    char nombre[128];
+    char horasTrabajadas[64];
+    char sueldo[64];
+    char id[64];
+    int sePudo=0;
+    Employee* empleado[TAM];
+    Employee* aux;
 
-    return 1;
+    len=ll_len(pArrayListEmployee);
+    i=len+1;
+    sprintf(id, "%d", i);
+    printf("Ingrese nombre ");
+    fflush(stdin);
+    scanf("%[^\n]",nombre);
+    printf("Ingrese sueldo ");
+    fflush(stdin);
+    scanf("%s",sueldo);
+    printf("Ingrese horas trabajadas ");
+    fflush(stdin);
+    scanf("%s",horasTrabajadas);
+    aux=employee_newParametros(id,nombre,horasTrabajadas,sueldo);
+    empleado[i]=aux;
+    ll_add(pArrayListEmployee,empleado[i]);
+    sePudo=1;
+    return sePudo;
 }
 
 
 int controller_editEmployee(LinkedList* pArrayListEmployee)
 {
+    Employee* aux;
+
+    char nombre[128];
+    char horasTrabajadas[64];
+    char sueldo[64];
+    char id[64];
+    int index;
+
+    printf("Quien va a modificar? ");
+    scanf("%s",id);
+    index=atoi(id);
+
+    printf("Ingrese nombre ");
+    fflush(stdin);
+    scanf("%[^\n]",nombre);
+    printf("Ingrese sueldo ");
+    fflush(stdin);
+    scanf("%s",sueldo);
+    printf("Ingrese horas trabajadas ");
+    fflush(stdin);
+    scanf("%s",horasTrabajadas);
+    aux=employee_newParametros(id,nombre,horasTrabajadas,sueldo);
+    ll_set(pArrayListEmployee,index-1,aux);
+
     return 1;
 }
 
 
 int controller_removeEmployee(LinkedList* pArrayListEmployee)
 {
-    return 1;
+    Employee* aux;
+
+    int index;
+    int i;
+    int len;
+    int sePudo=0;
+    printf("Quien va a borrar?");
+    scanf("%d",&index);
+    index--;
+    len=ll_len(pArrayListEmployee);
+    for(i=0;i<len;i++)
+    {
+        if(i==index)
+        {
+            aux = (Employee*) ll_get(pArrayListEmployee,index);
+            printf("(%6d) %6d %15s %8d %6d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+            ll_remove(pArrayListEmployee,index);
+            len=ll_len(pArrayListEmployee);
+            sePudo=1;
+        }
+    }
+    return sePudo;
 }
 
 
 int controller_ListEmployee(LinkedList* pArrayListEmployee)
 {
-    return 1;
+    int len=0;
+    int sePudo=0;
+
+    Employee* aux=NULL;
+    len=ll_len(pArrayListEmployee);
+    for(int i=0;i<len;i++)
+    {
+       aux = (Employee*) ll_get(pArrayListEmployee,i);
+       printf("(%6d) %6d %15s %8d %6d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+    }
+    printf("\n");
+    sePudo=1;
+
+    return sePudo;
 }
 
 
 int controller_sortEmployee(LinkedList* pArrayListEmployee)
 {
-    return 1;
+    int len;
+    int sePudo=0;
+
+    ll_sort(pArrayListEmployee,employee_CompareByName,1);//1 menor a mayor 0 mayor a menor
+    printf("Ordenados por Nombre\n");
+    controller_ListEmployee(pArrayListEmployee);
+    ll_sort(pArrayListEmployee,employee_CompareById,1);//1 menor a mayor 0 mayor a menor
+    printf("Ordenados por id\n");
+    controller_ListEmployee(pArrayListEmployee);
+    sePudo++;
+    return sePudo;
 }
 
 
 int controller_saveAsText(char* path, LinkedList* pArrayListEmployee)
 {
+    Employee* empleado=NULL;
+    FILE* pAux;
     int len;
-    fopen(path,"w");
-    len=ll_len(pArrayListEmployee);
-    fwrite(pArrayListEmployee,sizeof(Employee*),len,path);
-    fclose(path);
-    return 1;
-}
+    int i=0;
+    int aux;
+    int devolver=0;
+    pAux=fopen(path,"w");
 
+    if(pAux!=NULL)
+    {
+        len=ll_len(pArrayListEmployee);
+        aux=len-1;
+        while(!feof(pAux))
+        {
+            empleado=employee_new();
+            if(aux==i)
+            {
+                empleado=(Employee*) ll_get(pArrayListEmployee,i);
+                fprintf(pAux,"%d,%s,%d,%d",empleado->id,empleado->nombre,empleado->horasTrabajadas,empleado->sueldo);
+                devolver=1;
+                break;
+            }
+            else
+            {
+                empleado=(Employee*) ll_get(pArrayListEmployee,i);
+                fprintf(pAux,"%d,%s,%d,%d\n",empleado->id,empleado->nombre,empleado->horasTrabajadas,empleado->sueldo);
+                i++;
+            }
+        }
+    }
+    fclose(pAux);
+    return devolver;
+}
 
 int controller_saveAsBinary(char* path, LinkedList* pArrayListEmployee)
 {
+    Employee* empleado=NULL;
+    FILE* pAux;
     int len;
-    fopen(path,"wb");
-    len=ll_len(pArrayListEmployee);
-    fwrite(pArrayListEmployee,sizeof(Employee*),len,path);
-    fclose(path);
-    return 1;
+    int i=0;
+    int aux;
+    int devolver=0;
+    pAux=fopen(path,"wb");
+
+    if(pAux!=NULL)
+    {
+        len=ll_len(pArrayListEmployee);
+        aux=len-1;
+        while(!feof(pAux))
+        {
+            empleado=employee_new();
+            if(aux==i)
+            {
+                empleado=(Employee*) ll_get(pArrayListEmployee,i);
+                fprintf(pAux,"%d,%s,%d,%d",empleado->id,empleado->nombre,empleado->horasTrabajadas,empleado->sueldo);
+                devolver=1;
+                break;
+            }
+            else
+            {
+                empleado=(Employee*) ll_get(pArrayListEmployee,i);
+                fprintf(pAux,"%d,%s,%d,%d\n",empleado->id,empleado->nombre,empleado->horasTrabajadas,empleado->sueldo);
+                i++;
+            }
+        }
+    }
+    fclose(pAux);
+    return devolver;
 }
 
--- TP3/funciones.c
+++ TP3/funciones.c
@@ -16,14 +16,7 @@ void Menu(Employee* empleado[],LinkedList* miLista,Employee* aux)
 {
     int menu=0;
     int opcion;
-    int index;
-    int len;
-    int sueldoInt,horasTrabajadasInt;
-    char nombre[128];
-    char horasTrabajadas[64];
-    char sueldo[64];
-    char id[64];
-    int i=0;
+
     int banderaParsearArchivo=0;
     FILE* pArchivo;
     do
@@ -34,30 +27,18 @@ void Menu(Employee* empleado[],LinkedList* miLista,Employee* aux)
         {
             case 1:
                     pArchivo=fopen("data.csv","r");
-                    if(pArchivo==NULL)
+                    if(!(controller_loadFromText(pArchivo, miLista)==2))
                     {
-                        printf("Archivo incorrecto\n");
-                        break;
+                        printf("Error al leer el archivo");
                     }
-                    //llamar al parser desde el controlador
-
-                    if(!(parser_EmployeeFromText(pArchivo, miLista)))
-                    {
-                       printf("No se pudo cargar el archivo");
-                    }
-                    banderaParsearArchivo=1;
                     fclose(pArchivo);
+                    banderaParsearArchivo=1;
                 break;
             case 2:
-               pArchivo=fopen("data.csv","rb");
-                if(pArchivo==NULL)
+                pArchivo=fopen("data.csv","rb");
+                if(!(controller_loadFromBinary(pArchivo, miLista)==2))
                 {
-                    printf("Archivo incorrecto\n");
-                    break;
-                }
-                if(!(parser_EmployeeFromBinary(pArchivo, miLista)))
-                {
-                   printf("No se pudo cargar el archivo");
+                    printf("Error al leer el archivo");
                 }
                 banderaParsearArchivo=1;
                 fclose(pArchivo);
@@ -65,101 +46,76 @@ void Menu(Employee* empleado[],LinkedList* miLista,Employee* aux)
             case 3:
                 if(banderaParsearArchivo)
                 {
-                    len=ll_len(miLista);
-                    printf("%s","aaaaa");
-                    i=len+1;
-                    //strcpy(id[0],i);
-                    //id[0]=i;
-                    printf("Ingrese nombre");
-                    fflush(stdin);
-                    scanf("%[^\n]",nombre);
-                    printf("Ingrese sueldo");
-                    fflush(stdin);
-                    scanf("%s",sueldo);
-                    printf("Ingrese horas trabajadas");
-                    fflush(stdin);
-                    scanf("%s",horasTrabajadas);
-                    aux=employee_newParametros(id,nombre,horasTrabajadas,sueldo);
-                    empleado[i]=aux;
-                    ll_add(miLista,empleado[i]);
-
+                    if((!controller_addEmployee(miLista)))
+                    {
+                        printf("Error al agregar empleado\n");
+                    }
                 }
                 else
                 {
-                    printf("Carge el archivo primero");
+                    printf("Carge el archivo primero\n");
                 }
                 break;
             case 4://no anda Probar set o pop y push
                 if(banderaParsearArchivo)
                 {
-                    printf("Quien va a modificar?");
-                    scanf("%s",id);
-                    index=atoi(id);
-                    //employee_setId(empleado[i],i);
-                    printf("Ingrese nombre");
-                    fflush(stdin);
-                    scanf("%[^\n]",nombre);
-                    //employee_setNombre(empleado[i],nombre);
-                    printf("Ingrese sueldo");
-                    fflush(stdin);
-                    scanf("%s",sueldo);
-                    //sueldoInt=atoi(sueldo);
-                    //employee_setSueldo(empleado[i],sueldoInt);
-
-                    printf("Ingrese horas trabajadas");
-                    fflush(stdin);
-                    scanf("%s",horasTrabajadas);
-                    //horasTrabajadasInt=atoi(horasTrabajadas);
-                    //employee_setHorasTrabajadas(empleado[i],horasTrabajadasInt);
-                    aux=employee_newParametros(id,nombre,horasTrabajadas,sueldo);
-                    ll_set(miLista,index-1,aux);
+                    controller_editEmployee(miLista);
+                }
+                else
+                {
+                    printf("Carge el archivo primero\n");
                 }
                 //empleado[i]->horasTrabajadas=atoi(horasTrabajadas);
                 //USAR SETTER
                 break;
-            case 5://NO ANDA EL MOSTRAR, CHECKEAR SI EXISTE EL QUE QUIERE BORRARSE
-                //int elementoBorrado;
-                printf("Quien va a borrar?");
-                scanf("%d",&index);
-                //1elementoBorrado=employee_getId(miLista,index-1);
-                ll_remove(miLista,index-1);
-                len=ll_len(miLista);
-                printf("Borrando elemento seleccionado\n");
-                //aux = (Employee*) ll_get(miLista,index-1);
-                //printf("(%d) %d %s %d %d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+            case 5://esto anda de maravilla
+                if(banderaParsearArchivo)
+                {
+                    if(!(controller_removeEmployee(miLista)))
+                    {
+                        printf("No se pudo borrar el elemento seleccionado\n");
+                    }
+                    else
+                    {
+                        printf("Borrando elemento seleccionado\n");
+                    }
+                }
+                else
+                {
+                    printf("Carge el archivo primero\n");
+                }
                 break;
             case 6:
-                    len=ll_len(miLista);
-                    printf("\nLOS PIBES\n");
-                    for(int i=0;i<len;i++)
+                if(banderaParsearArchivo)
+                {
+                    if(!(controller_ListEmployee(miLista)))
                     {
-                       aux = (Employee*) ll_get(miLista,i);
-                       printf("(%d) %d %s %d %d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+                        printf("No se pudo mostrar\n");
                     }
-                    printf("\n");
+                }
+                else
+                {
+                    printf("Carge el archivo primero\n");
+                }
                 break;
             case 7:
-                ll_sort(miLista,employee_CompareByName,1);//1 menor a mayor 0 mayor a menor
-                len=ll_len(miLista);
-                printf("Ordenados por Nombre\n");
-                for(int i=0;i<len;i++)
+                if(banderaParsearArchivo)
                 {
-                   aux = (Employee*) ll_get(miLista,i);
-                   printf("(%d) %d %s %d %d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+                    controller_sortEmployee(miLista);
                 }
-                ll_sort(miLista,employee_CompareById,1);//1 menor a mayor 0 mayor a menor
-                printf("Ordenados por id\n");
-                for(int i=0;i<len;i++)
+                else
                 {
-                   aux = (Employee*) ll_get(miLista,i);
-                   printf("(%d) %d %s %d %d\n",i+1,aux->id,aux->nombre,aux->sueldo,aux->horasTrabajadas);
+                    printf("Carge el archivo primero\n");
                 }
                 break;
             case 8://no funciona
-                controller_saveAsText(pArchivo, miLista);
+                if(!(controller_saveAsText("data.csv", miLista)))
+                   {
+                       printf("\nError al guardar en el archivo\n");
+                   }
                 break;
             case 9:
-                controller_saveAsBinary(pArchivo, miLista);
+                controller_saveAsBinary("data.csv", miLista);
                 break;
             case 10:
                 menu=1;
